80b42368e75c4d442536e7996c375d40893ce2d8
lomaTT <alexeyttdeveloper@gmail.com>
2023-06-29 05:32:57 +0200

    header realization

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,5 +1,10 @@
-import Game from "../components/game/Game";
+import { Header } from "../components/Header"
 
 export default function HomePage() {
-    return <Game />;
+    // return <Game />;
+    return (
+        <div className="bg-slate-500 min-h-screen">
+            <Header />
+        </div>
+    );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,11 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      lineHeight: {
+        tight: "1.2",
+      },
+    },
   },
   plugins: [],
 };
